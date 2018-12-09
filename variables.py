9493b5eae39d83ce4c04d07ec36fe09b05c0ea2f
Francisco Sobrinho <francicsobrinho@gmail.com>
2018-12-09 14:31:33 -0200

    Retirada de linhas de testes e inclusão de funcionalidade

--- variables.py
+++ variables.py
@@ -1,5 +1,10 @@
-def step2_funcion(arg1,arg2):
-	print("Running funcion step2_funcion with args {} e {}".format(arg1,arg2))
-step1_file = "runner1.py"
-value1_dinamic = "value1_25645"
-value2_dinamic = "value2_sdfsd"
+# Utils functions
+
+# Variables
+variables = {
+	
+}
+# Exported functions
+def format_variable(cmd_str):
+	return cmd_str.format(variables)
+	
