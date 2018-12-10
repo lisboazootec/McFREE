5d159ce22af3b709fc84ceaf3256c39ebfce7c36
Francisco Sobrinho <francicsobrinho@gmail.com>
2018-12-09 22:19:32 -0200

    Step como um classe dict especial

--- main.py
+++ main.py
@@ -15,43 +15,41 @@ class Variables:
 	@classmethod
 	def format_files(cls,args_str):
 		try:
-			return args_str.format(**cls._files)
+			return args_str.format_map(cls._files)
 		except KeyError as e:
 			e.args = ("Referência de arquivo não encontrado: %s"%str(e),)
 			raise e
 
-class PipelineStep:
+class PipelineStep(dict):
+
+	def __missing__(self,key):
+		return ''
 
 	def __init__(self,step):
-
-		# Required values
-		self.index = step['index']
 		self.runner = Variables.get_runner(step['runner'])
-		self.arguments = Variables.format_files(step['args'])
-
-		# Optional values
-		self.name = step.get('name') or ''
-		self.description = step.get('description') or ''
+		self.args = Variables.format_files(step['args'])
+		step['args'] = self.args
+		self.update(step)
 
 	def __str__(self):
-		return "{name}({index}): {runner} {arguments}".format(**self.__dict__)
+		return "{name}({index}): {runner} {args}".format_map(self)
 
 	__repr__ = __str__
 
 	def run(self):
+		print('Running',self)
 		if type(self.runner)==str:
 			self._runos()
 		else:
 
-			self.arguments = shlex.split(self.arguments)
+			self.args = shlex.split(self.args)
 
 			parser = argparse.ArgumentParser()
-			[parser.add_argument(arg) for arg in self.arguments if arg.startswith('-')]
-			args = parser.parse_args(self.arguments)
+			[parser.add_argument(arg) for arg in self.args if arg.startswith('-')]
+			args = parser.parse_args(self.args)
 			self.runner(**vars(args))
 	def _runos(self):
-		command = "{} {}".format(self.runner,self.arguments)
-		print('Execution command on OS:',command)
+		command = "{runner} {args}".format_map(self)
 		os.system(command)
 
 pipeline = [PipelineStep(step) for step in steps]
