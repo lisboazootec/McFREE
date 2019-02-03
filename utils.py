ea7bb67482ab6ddd3837d7db7489ce3741129a1f
Diego <lisboa.zootec@gmail.com>
2019-02-03 15:57:50 -0200

    Adição de steps
    Feito com Diego em Divinópolis

--- utils.py
+++ utils.py
@@ -2,4 +2,16 @@ __all__ = [
 ]
 
 def fastq_join_remains(unpared1_file,unpared2_file,merged_file,output_file):
+	'''
+		Agrupamento de reads fowrard e reverse pelo ID
+	'''
+	pass
+
+def fastq_uniq_seq(grouped_fastq,uniq_fastq):
+	'''
+		Retirar sequências redundantes do arquivo de sequências agrupadas
+	'''
+	pass
+
+def mapfilter_longest_orfs(input_file,output_file):
 	pass
--- variables.py
+++ variables.py
@@ -11,10 +11,15 @@ files = {
 	
 	'data':'{root}/data', # Contém todos os arquivos de dados utilizados na pipeline
 	'data_fastQJoin':'{data}/FastQJoin'
+	'data_fastQuniq':'{data}/FastQUniq'
 	'metaseq':'{data}/Metatranscriptome_sequences', # Contém os arquivos de sequencias brutas
 	'databases':'{data}/Databases',
-	
-	'trinity':'{lib}/Trinity',
+	'data_HSBLASTn':'{data}/HSBLASTn'
+	'data_Trinity':'{data}/Trinity'
+	'data_TransDecoder':'{data}/TransDecoder'
+	'lib_trinity':'{lib}/Trinity',
+	'lib_lca':'{lib}/LCA',
+	'lib_transDecoder':'{lib}/TransDecoder'
 
 	#### INPUT FILES
 
@@ -24,11 +29,20 @@ files = {
 	'mergedFastQ':'{data_fastQJoin}/merged.fastq',
 	'unpared1_fastq':'{data_fastQJoin}/unpared1.fastq'.
 	'unpared2_fastq':'{data_fastQJoin}/unpared2.fastq',
-	'final_fastq':'{data_fastQJoin}/final.fastq',
+	'grouped_fastq':'{data_fastQJoin}/grouped.fastq',
 
 	'proteinACC_Accession':'{databases}/proteinacc2acession.tsv', # ?
 	'nomReduant_diamond':'{databases}/nr_dmnd.db', # Dados de proteínas não redundantes do NCBI no formato para o Diamond
 	'nomReduant_kegg':'{databases}/nr_kegg.db', # Relacionamento de proteínas não redundantes do NCBI com o KEGG Ortológico
+	
+	'list_redudants':'{data_fastQuniq}/list_redudants.json',
+	'uniq_fastq':'{data_fastQJoin}/uniq.fastq',
+	'refseq_fna':'{databases}/refseq.fna', # 
+	'refseq_fna_counts_obinary':'{databases}/refseq.fna.counts.obinary', # 
+	'blastn_fa':'{data_HSBLASTn}/blastn.fa',
+	'trinity_fa':'{data_Trinity}/Trinity.fasta', 
+	'trinity_fa':'{data_Trinity}/Trinity.fasta', 
+	'longestOrfs':'{data_TransDecoder}/longest_orfs.pep'
 
 	#### OUTPUT FILES
 
@@ -36,6 +50,7 @@ files = {
 
 	'cpu_limit':'64',
 	'memory_limit':'200',
+	'max_cov': '5' #deve ser modificado
 
 
 }
@@ -53,12 +68,55 @@ steps = [
 		"name": "Juntar as reads não unidas",
 		'index':"s0220"
 		'runner':'fastQJoinRemains',
-		'args':'--unpared1-file {unpared1_fastq} --unpared2-file {unpared2_fastq} --merged-file {mergedFastQ} --output-file {final_fastq}',
+		'args':'--unpared1-file {unpared1_fastq} --unpared2-file {unpared2_fastq} --merged-file {mergedFastQ} --output-file {grouped_fastq}',
 		'outputs':[]
-	}
+	},{
+		"name": "Retirar as sequencias redundantes",
+		'index':"s0230"
+		'runner':'fastqUniqSeq',
+		'args':'--redundant-file {grouped_fastq} --output-file {uniq_fastq}',
+		'outputs':['{list_redudants}']
+	},{
+		"name": "MegaBLAST",
+		'index':"s0240"
+		'runner':'HSBLASTn',
+		'args':'-db {refseq_fna} -window_masker_db {refseq_fna_counts_obinary} -query {uniq_fastq} -out {blastn_fa} -outfmt 7',
+		'outputs':[]
+	},{
+		"name": "Lowest Commumn Ancestor",
+		'index':"s0250"
+		'runner':'lca',
+		'args': #temos de rever os argumentos
+		'outputs':[]
+	},{
+		"name": "Contig Assembly (Trinity)",
+		'index':"s0260"
+		'runner':'trinity',
+		'args':'--seqType fq --SS_lib_type FR --normalize_max_read_cov {max_cov} --left {fSeqFastQ} --right {rSeqFastQ} --CPU {cpu_limit} --output {trinity_fa} --trimmomatic "ILLUMINACLIP:$TRIMMOMATIC_DIR/adapters/TruSeq3-PE.fa:2:30:10 HEADCROP:10 CROP:285 LEADING:10 TRAILING:5 SLIDINGWINDOW:4:15 MINLEN:25"',
+ 		"description": "Inicio da análise funcional"
+		'outputs':[]
+	},{
+		"name": "Identificação CDS",
+		'index':"s0270"
+		'runner':'transDecoder',
+		'args':'-t {trinity_fa} -m {memory_limit}',
+		'outputs':['{longestOrfs}']
+	},{
+		"name": "Selecionando candidatos para treino/Buscando evidências de candidatos",
+		'index':"s0280"
+		'runner':'mapfilter_longest_orfs',
+		'args':'--input-file {longestOrfs} --output-file {complete_candidates}',
+		'outputs':[]
+	},
 ]
 
 runners = {
 	'fastQJoin':'{lib_fastQJoin}/clipper/fastq-join', # 
 	'fastQJoinRemains':fastq_join_remains,
+	'fastqUniqSeq': fastq_uniq_seq
+	'HSBLASTn':'HS-BLASTn',
+	'lca':'{lib_lca}/lca.py',
+	'trinity':'{lib_trinity}/[trinity_runner]',
+	'transDecoder':'{lib_transDecoder}/TransDecoder.LongOrfs'
+	'mapFilterLongestOrfs':mapfilter_longest_orfs
 }
