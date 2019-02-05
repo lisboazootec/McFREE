1605568c18dd5217fc13edf614a62736d4a0628b
Francisco Sobrinho <francicsobrinho@gmail.com>
2019-02-04 22:21:59 -0200

    Finalização da inserção dos steps

--- variables.py
+++ variables.py
@@ -58,56 +58,160 @@ files = {
 steps = [
 	{
 		"name": "Junta as reads reverse e forward",
-		'index':"s0210"
+		'index':"s0210",
 		'runner':'fastQJoin',
 		'args':'-p {cpu_limit} -m {memory_limit} {fSeqFastQ} {rSeqFastQ} -o {mergedFastQ}',
 		'outputs':['{unpared1_fastq}','{unpared2_fastq}'],
- 		"description": "--"
+ 		"description": "--",
 	},
 	{
 		"name": "Juntar as reads não unidas",
-		'index':"s0220"
+		'index':"s0220",
 		'runner':'fastQJoinRemains',
 		'args':'--unpared1-file {unpared1_fastq} --unpared2-file {unpared2_fastq} --merged-file {mergedFastQ} --output-file {grouped_fastq}',
 		'outputs':[]
 	},{
 		"name": "Retirar as sequencias redundantes",
-		'index':"s0230"
+		'index':"s0230",
 		'runner':'fastqUniqSeq',
 		'args':'--redundant-file {grouped_fastq} --output-file {uniq_fastq}',
 		'outputs':['{list_redudants}']
 	},{
 		"name": "MegaBLAST",
-		'index':"s0240"
+		'index':"s0240",
 		'runner':'HSBLASTn',
 		'args':'-db {refseq_fna} -window_masker_db {refseq_fna_counts_obinary} -query {uniq_fastq} -out {blastn_fa} -outfmt 7',
 		'outputs':[]
 	},{
 		"name": "Lowest Commumn Ancestor",
-		'index':"s0250"
+		'index':"s0250",
 		'runner':'lca',
 		'args': #temos de rever os argumentos
 		'outputs':[]
 	},{
 		"name": "Contig Assembly (Trinity)",
-		'index':"s0260"
+		'index':"s0260",
 		'runner':'trinity',
 		'args':'--seqType fq --SS_lib_type FR --normalize_max_read_cov {max_cov} --left {fSeqFastQ} --right {rSeqFastQ} --CPU {cpu_limit} --output {trinity_fa} --trimmomatic "ILLUMINACLIP:$TRIMMOMATIC_DIR/adapters/TruSeq3-PE.fa:2:30:10 HEADCROP:10 CROP:285 LEADING:10 TRAILING:5 SLIDINGWINDOW:4:15 MINLEN:25"',
- 		"description": "Inicio da análise funcional"
+ 		"description": "Inicio da análise funcional",
 		'outputs':[]
 	},{
 		"name": "Identificação CDS",
-		'index':"s0270"
+		'index':"s0270",
 		'runner':'transDecoder',
 		'args':'-t {trinity_fa} -m {memory_limit}',
 		'outputs':['{longestOrfs}']
 	},{
 		"name": "Selecionando candidatos para treino/Buscando evidências de candidatos",
-		'index':"s0280"
-		'runner':'mapfilter_longest_orfs',
-		'args':'--input-file {longestOrfs} --output-file {complete_candidates}',
+		'index':"s0280",
+		'runner':'grep_sed_cut',
+		'args':'grep "complete" {longestOrfs} | sed -e "s/>//g" | cut -d " " -f1 > {complete_candidates}',
 		'outputs':[]
-	},
+	},{
+		"name": "",
+		'index':"passo7c1",
+		'runner':'makeblastdb',
+		'args':"-dbtype 'prot' -in {longestOrfs} -parse_seqids -out {predicted}",
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"passo7c2",
+		'runner':'blastdbcmd',
+		'args':"-db {predicted} -entry_batch {complete_candidates} > {complete_fasta}",
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"passo7c2",
+		'description':"sed -e 's/lcl|//g'",
+		'runner':'replace_lcl',
+		'args':"--input-file {complete_fasta} --output-file {complete_fasta}",
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"passo7d1",
+		'runner':'diamond',
+		'args':'-d {nr_dmnd} -q {complete_fasta} -a {complete_blastp} -e 1e-6 -p 64 -f "6 qacc sacc pident qlen slen length qcovs evalue bitscore" -k 1',
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"passo7d2",
+		'runner':'diamond',
+		'args':'view -a {complete_blastp_daa} -o {complete_blastp_m8}',
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"passo7e1",
+		'runner':'cat_cut_sort',
+		'args':'{complete_blastp_m8} | cut -f1 | sort -u > {evidences_list}',
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"passo7e2",
+		'runner':'makeblastdb',
+		'args':"-dbtype 'nucl' -in {longestOrfs} -parse_seqids -out {cds}",
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"passo7e3",
+		'runner':'blastdbcmd',
+		'args':"-db {cds} -entry_batch {evidences_list} | sed -e 's/lcl|//g' > {train_fasta}",
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"passo7f",
+		'runner':'transDecoder',
+		'args':"-t {trinity_fa} --cpu 4 --train train.fasta",
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"passo7g1",
+		'runner':'diamond',
+		'args':'blastp -d {nr_dmnd} -q {trinity_fasta_transdecoder_pep} -a {final_blastp} -e 1e-6 -p 64 -f "6 qacc sacc pident qlen slen length qcovs evalue bitscore" -k 1',
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"passo7g2",
+		'runner':'diamond',
+		'args':'view -a {final_blastp_daa} -o {final_blastp_m8}',
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"passo8b",
+		'runner':'star',
+		'args':'--runMode genomeGenerate --runThreadN 64 --genomeDir {reference} --limitGenomeGenerateRAM=80000000000 --genomeFastaFiles {trinity_fasta_transdecoder_cds}',
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"passo8c",
+		'runner':'star',
+		'args':'--runMode genomeGenerate --runThreadN 64 --genomeDir {reference} --readFilesIn {f_fastq} {f_fastq} --outFileNamePrefix {star_map} --outSAMtype BAM SortedByCoordinate',
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"",
+		'runner':'samtools',
+		'args':"view {star_map_aligned} > {reads}",
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"",
+		'description':"cut -f1,3",
+		'runner':'cut',
+		'args':"--input-file {reads} --output-file {reads_id}",
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"passo10",
+		'runner':'script_groupko',
+		'args':"{final_blastp_m8} {reads_id} {blastn_fa}",
+		'outputs':[]
+	},{
+		"name": "",
+		'index':"",
+		'runner':'mysql',
+		'args':"-h {db_hostname} -D {db_db} -u {db_user} -p < {query_sql} > {lib}.final.tsv",
+		'outputs':[]
+	}
 ]
 
 runners = {
